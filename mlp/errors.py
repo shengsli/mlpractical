94864e6693e8d916dc1a47973a3139667acbac3d
shengli <s1837296@sms.ed.ac.uk>
2018-10-13 15:46:03 +0100

    lab2

--- mlp/errors.py
+++ mlp/errors.py
@@ -25,7 +25,7 @@ class SumOfSquaredDiffsError(object):
         Returns:
             Scalar error function value.
         """
-        raise NotImplementedError()
+        return 0.5 * ((outputs-targets)**2).sum()/outputs.shape[0]
 
     def grad(self, outputs, targets):
         """Calculates gradient of error function with respect to outputs.
@@ -38,7 +38,7 @@ class SumOfSquaredDiffsError(object):
             Gradient of error function with respect to outputs. This should be
             an array of shape (batch_size, output_dim).
         """
-        raise NotImplementedError()
+        return (outputs-targets)/outputs.shape[0]
 
     def __repr__(self):
         return 'SumOfSquaredDiffsError'
