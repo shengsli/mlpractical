e0092b3f4f63e8d5a7e87fcc505e5f626282a41b
shengli <s1837296@sms.ed.ac.uk>
2018-11-23 00:32:51 +0000

    update

---
+++ old_stats/num_layers/plot.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+# stats_dict = {
+#     "train_acc": data[:,0],
+#     "train_loss": data[:,1],
+#     "val_acc": data[:,2],
+#     "val_loss": data[:,3]
+# }
+
+dim_reduction = "avg_pooling"
+fig_title = "Val loss of max and avg pooling"
+save_fig_name = 'pooling.pdf'
+
+run_times = 2
+all_data = np.empty((run_times,100,4))
+all_total_time = np.empty((run_times))
+all_best_model_data = np.empty((run_times,2))
+all_best_val_epoch = np.empty(run_times)
+for run in range(1,3):
+    all_data[run-1,:,:] = np.loadtxt(
+        'avg_pooling_4layers/{0}run/result_outputs/summary.csv'.format(run),delimiter=',', skiprows=1)
+    all_best_model_data[run-1,:] = np.loadtxt(
+        'avg_pooling_4layers/{0}run/result_outputs/test_summary.csv'.format(run),
+        delimiter=',', skiprows=1)
+avg_data = all_data.mean(axis=0)
+avg_best_val_epoch = np.median(np.argmin(all_data[:,:,3], axis=1))
+avg_best_model_data = all_best_model_data.mean(axis=0)
+print(avg_best_val_epoch)
+print(avg_best_model_data)
+plt.plot(avg_data[:,3], label='avg pooling')
+
+###############################
+dim_reduction = "avg_pooling"
+
+run_times = 2
+all_data = np.empty((run_times,100,4))
+all_total_time = np.empty((run_times))
+all_best_model_data = np.empty((run_times,2))
+all_best_val_epoch = np.empty(run_times)
+for run in range(1,3):
+    all_data[run-1,:,:] = np.loadtxt(
+        'max_pooling_4layers/{0}run/result_outputs/summary.csv'.format(run),delimiter=',', skiprows=1)
+    all_best_model_data[run-1,:] = np.loadtxt(
+        'max_pooling_4layers/{0}run/result_outputs/test_summary.csv'.format(run),
+        delimiter=',', skiprows=1)
+avg_data = all_data.mean(axis=0)
+avg_best_val_epoch = np.median(np.argmin(all_data[:,:,3], axis=1))
+avg_best_model_data = all_best_model_data.mean(axis=0)
+print(avg_best_val_epoch)
+print(avg_best_model_data)
+plt.plot(avg_data[:,3], label='max pooling')
+
+
+plt.legend()
+plt.title(fig_title)
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.savefig(save_fig_name)
+plt.show()
